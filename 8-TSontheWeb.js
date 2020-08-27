fec340a542959c5fc3de951e5a5aace26724f9a2
Gabrielle Crevecoeur <gcrev93@gmail.com>
2020-08-27 13:08:01 -0400

    Delete 8-TSontheWeb.js

--- 8-TSontheWeb.js
+++
@@ -1,9 +0,0 @@
-/*
-
-*/
-var newShape5 = {
-    width: 10,
-    height: 15,
-    shapeArea2: function (x, y) { return x * y; }
-};
-document.body.innerHTML = "This is the area " + newShape5.shapeArea2(newShape5.height, newShape5.width);
